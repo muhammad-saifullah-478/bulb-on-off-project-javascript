5e14b3da9abe77487054d2da9c8acfb05aca5674
muhammad-saifullah-478 <168011894+muhammad-saifullah-478@users.noreply.github.com>
2024-08-06 23:25:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById("img").src="of.png"
+
+
+
+function btnon(){
+    document.getElementById("img").src="on.png"
+}
+function btnof(){
+    document.getElementById("img").src="of.png"
+}
